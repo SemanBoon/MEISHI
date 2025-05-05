5271ed5189f10af092cdace7366cd6d1075e3022
bhavanachem <chemuturib@gmail.com>
2025-05-04 23:00:22 -0500

    readme3

--- Frontend/pages/auth.js
+++ Frontend/pages/auth.js
@@ -1,3 +1,7 @@
+// written by: Alejandro
+// tested by: Alejandro
+// debugged by: Alejandro
+
 document.addEventListener("DOMContentLoaded", () => {
   const signupForm = document.getElementById("signup-form");
   const loginForm = document.getElementById("login-form");
